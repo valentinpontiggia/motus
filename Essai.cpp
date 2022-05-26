18e93be4b516d75189de3759c34bd501b8ba3f20
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-26 17:08:16 +0200

    Update Essai.cpp
    la fonction comparer est là pour colorier les cases mais tu peux l'enlever si jamais on a pas besoin
    la fonction Existence vérifie si le mot est dans le dico, à vérifier

--- Essai.cpp
+++ Essai.cpp
@@ -1,13 +1,15 @@
 #pragma once
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 #include "Word.h"
 #include "Essai.h"
 #include "Solution.h"
 #include <string>
-
+//peut-être trop d'include mais on pourra supprimer le surplus plus tard
 using namespace std;
 
 void Essai::Colorier(Essai Mot)
@@ -17,14 +19,31 @@ void Essai::Colorier(Essai Mot)
 
 void Essai ::Comparer(Essai name_, Solution mot_compare)
 {
-	int tab[name_.size()];
-	for (int i = 0, i < name_.size(), i++)
+	vector <int> tab(name_.getNom().size());
+	for (int i = 0; i < name_.getNom().size(); i++)
 	{
 		if (name_[i] == mot_compare[i])
 			tab[i] = 2;
-		if ((name_[i] != mot_compare[i]) && (mot_compare[i] == name_.find(mot_compare[i])))
+		if ((name_[i] != mot_compare[i]) && (mot_compare[i] == name_.getNom().find(mot_compare[i])))
 			tab[i] = 1;
 		else
 			tab[i] = 0;
 	}
+}
+
+void Essai::Existence(Essai Mot)
+{
+	int max = 323578;
+	string mot_compare;
+	ifstream file;
+	file.open("test.txt");
+	if (!file.is_open())
+		cout << "Le fichier n existe pas " << endl;
+	else
+		while (getline(file, mot_compare)) //lit 1 par 1 les lignes du fichier et met cette ligne dans mot_compare
+		{
+			if (mot_compare == Mot.getNom()) //on compare le mot de la ligne au mot testé
+			break;
+		}
+	cout << "Ce mot n existe pas !" << endl; //si aucun des mots du fichier n'est égal au mot testé alors le mot testé n'existe pas
 }
