22131e73955f730c491cd03ec73e6ff9b59e449f
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-26 17:08:59 +0200

    Update Essai.h

--- Essai.h
+++ Essai.h
@@ -3,11 +3,12 @@
 #include "Word.h"
 #include "Solution.h"
 using namespace std;
-
+// Contient les mots à tester par le joueur
 class Essai : public Word
 {
 	Word test_;
 public :
 	void Colorier(Essai mot_compare);
 	void Comparer(Essai mot_compare, Solution solution);
+	void Existence(Essai Mot);
 };
