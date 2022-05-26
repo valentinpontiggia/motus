c8a874c80469e593c7e8c69cb3b6e43c779434e9
chloedavn <104068806+chloedavn@users.noreply.github.com>
2022-05-26 17:09:23 +0200

    Update graphic.cpp

--- graphic.cpp
+++ graphic.cpp
@@ -83,13 +83,14 @@ void drawword(RenderWindow &window, Word word, Word w2, int p1, int p2, Text tex
 			// Demande de fermeture de la fenêtre
 			if (event.type == sf::Event::Closed)
 				window.close();
-			// Affichage d'un mot
+			// Affichage d'un mot tapé sur le clavier
 			if (event.type == sf::Event::TextEntered)
 			{
 
 				if (event.text.unicode < 128)
 				{
 					c = 0;
+					// On récupère les données des touches du clavier
 					s = static_cast<char>(event.text.unicode);
 					s3 += static_cast<char>(event.text.unicode);
 					// transformation de la chaine en majuscule
@@ -118,6 +119,7 @@ void drawword(RenderWindow &window, Word word, Word w2, int p1, int p2, Text tex
 
 			}
 
+			// Evenement si on appui sur la touche entrer (ca pass a la ligne suivante et ca permet d'afficher les lettres bien ou mal placées)
 			if (event.key.code == sf::Keyboard::Enter)
 			{
 				p2 = p2 + 58;
@@ -133,7 +135,7 @@ void drawword(RenderWindow &window, Word word, Word w2, int p1, int p2, Text tex
 
 			}
 
-
+			// Permet d'effacer un caractère rentré sur le clavier avec la touche backspace ou suppr
 			if (event.key.code == sf::Keyboard::BackSpace || event.key.code == sf::Keyboard::Delete)
 
 			{
@@ -145,30 +147,33 @@ void drawword(RenderWindow &window, Word word, Word w2, int p1, int p2, Text tex
 			}
 
 		}
+		// Si c = 1 on veut effacer le dernier caractère rentré
+		// Efface le caractère en mettant un carré noir sur la fenêtre
 		if (c == 1)
 		{
 
 			window.draw(carre2);
 			drawgrid(window);
 		}
-		else
+		else // sinon on affiche le dernier caractère rentré
 		{
 			window.draw(text);
 		}
-		// Verification de la  premiere ligne
 
-		//cout << s2;
+		//On verifie que la chaine rentré est bien de 6 lettres
 		if (s2.size() == 6)
 		{
+			// Si on a bien validé le mot rentré avec la touche "entrer", b est à 1
 			if (b == 1)
 			{
+				// On verifie alors si il y a des lettres mal placées
 				for (int k = 0; k < 6; k++)
 				{
 					for (int l = 0; l < 6; l++)
 					{
 						if (s2[k] == s4[l])
 						{
-							drawreplace(window, p3 + k * 116, p7);
+							drawreplace(window, p3 + k * 116, p7); // Affiche par un rond jaune les lettres mal placées
 							text.setString(s2[k]);
 							text.setPosition(p9 + k * 116, p8);
 							window.draw(text);
@@ -176,13 +181,14 @@ void drawword(RenderWindow &window, Word word, Word w2, int p1, int p2, Text tex
 					}
 
 				}
+				// On verifie s'il y a des lettres bien placées
 				for (int i = 0; i < 6; i++)
 				{
 
 					if (s2[i] == s4[i])
 					{
 
-						drawright(window, p3 + i * 116, p4);
+						drawright(window, p3 + i * 116, p4); // Affiche par un carré rouge les lettres bien placées
 						text.setString(s2[i]);
 						r = r + 1;
 						text.setPosition(p9 + i * 116, p6);
@@ -192,13 +198,13 @@ void drawword(RenderWindow &window, Word word, Word w2, int p1, int p2, Text tex
 						r = 0;
 				}
 			}
-				drawgrid(window);
+			drawgrid(window);
 		}
 		window.display();
 	}
 }
 
-void drawright(RenderWindow &window, int p2, int p3)
+void drawright(RenderWindow &window, int p2, int p3) // fonction qui crée un carré rouge
 {
 	RectangleShape carre(Vector2f(117, 117));
 	carre.setFillColor(Color::Red);
@@ -206,7 +212,7 @@ void drawright(RenderWindow &window, int p2, int p3)
 	window.draw(carre);
 }
 
-void drawreplace(RenderWindow &window, int p2, int p3)
+void drawreplace(RenderWindow &window, int p2, int p3) // fonction qui crée un rond jaune
 {
 	CircleShape circle(55);
 	circle.setFillColor(Color::Yellow);
@@ -214,7 +220,7 @@ void drawreplace(RenderWindow &window, int p2, int p3)
 	window.draw(circle);
 }
 
-string MinToMaj(string s)
+string MinToMaj(string s) // fonction qui fait conerti une chaine minuscule en majuscule
 {
 	for (int i = 0; s[i] != '\0'; i++)
 	{
