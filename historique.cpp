6ab152e34ef95480f443b2761627746de2aa0e43
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-30 11:09:24 +0200

    Update historique.cpp

--- historique.cpp
+++ historique.cpp
@@ -3,13 +3,14 @@
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
 #include "historique.h"
+#include"menu.h"
 using namespace sf;
 using namespace std;
 
 void Historique :: ecrireHistorique(int nb_essais)
 {
 	ofstream historique("historique.txt");
-	historique << to_string(nb_essais) << endl;
+	historique << to_string(nb_essais) << endl; // rentre nb_essais dans le fichier historique.txt
 	historique.close();
 }
 
@@ -42,24 +43,28 @@ Historique::Historique(float moyenne, float largeur, float hauteur)
 	string nb_essais;
 	string strmoyenne(to_string(moyenne));
 	file.open("historique.txt");
-	while (getline(file, nb_essais))
-	{
-	}
+	getline(file, nb_essais); // Récupère la première ligne du fichier historique.txt (= le dernier nb de tentatives)
+
 	historique[0].setFont(font);
 	historique[0].setFillColor(Color::Red);
 	historique[0].setCharacterSize(45);
 	historique[0].setString("~ Historique ~");
-	historique[0].setPosition(Vector2f(270, 120));
+	historique[0].setPosition(Vector2f(220, 120));
 
 	historique[1].setFont(font);
 	historique[1].setFillColor(Color::White);
-	historique[1].setString("Dernier nombres de tentative :"+nb_essais);
-	historique[1].setPosition(Vector2f(290, 320));
+	historique[1].setString("Dernier nombres de tentative : "+nb_essais);
+	historique[1].setPosition(Vector2f(100, 320));
 
 	historique[2].setFont(font);
 	historique[2].setFillColor(Color::White);
-	historique[2].setString("Moyenne de tentatives :"+strmoyenne);
-	historique[2].setPosition(Vector2f(270, 450));
+	historique[2].setString("Moyenne de tentatives : "+strmoyenne);
+	historique[2].setPosition(Vector2f(100, 450));
+
+	historique[3].setFont(font);
+	historique[3].setFillColor(Color::White);
+	historique[3].setString("~ Retour au menu ~");
+	historique[3].setPosition(Vector2f(220, 580));
 }
 
 
@@ -69,17 +74,35 @@ Historique::~Historique()
 
 void Historique::afficher(RenderWindow& window)
 {
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 4; i++)
 	{
 		window.draw(historique[i]);
 	}
 }
 
-void Historique::afficherHistorique(Historique historique)
+void Historique::afficherHistorique(Historique historique, RenderWindow& window)
 {
-	RenderWindow window(VideoMode(700, 700), "MOTUS");
-	cout << "test reussi" << endl;
-	window.clear();
-	historique.afficher(window);
-	window.display();
+	RenderWindow window1; // Nouvelle fenêtre pour l'historique
+	Event event;
+	window.close(); // Fermeture du menu
+	window1.create(VideoMode(700, 700), "MOTUS");
+	window1.clear();
+	historique.afficher(window1);
+	window1.display();
+	while (window1.isOpen())
+	{
+		while (window1.pollEvent(event))
+		{
+			if (event.type == Event::MouseButtonPressed)
+				if (event.mouseButton.x >= 200 && event.mouseButton.x <= 600)
+				{
+					if (event.mouseButton.y >= 550 && event.mouseButton.y <= 610) // Si on clique sur "Retour au menu"
+					{
+						window1.close();
+						Menu menu(700, 700);
+						menu.afficherMenu(menu);
+					}
+				}
+		}
+	}
 }
