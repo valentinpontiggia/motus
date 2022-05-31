f6289ca9a7df2c381ed157d02c478cf8781814c7
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-31 17:39:28 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -5,13 +5,14 @@ using namespace sf;
 class Menu
 {
 	Font font;
-	Text menu[4];
+	Text menu[4]; //Un pour le titre et un pour chaque option du menu
 
 public:
-	Menu(float hauteur, float largeur);
+	Menu(float hauteur, float largeur); //construit le menu et remplit les champs des menu[i]
 	~Menu();
-	void afficherMenu(Menu menu);
-	void afficher(RenderWindow& window);
+	void afficher(RenderWindow& window); //draw les champs du menu dans la fenêtre, sans l'afficher.
+	void afficherMenu(Menu menu); //ouvre la fenêtre contenant le menu et gère les clics de la souris pour ouvrir les différentes options du menu
 
 };
 
+Text textPointed(RenderWindow& window, Text& text); //change la couleur du texte s'il est pointé (ne fonctionne pas)
