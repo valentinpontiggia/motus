5939b1fd5e3977be08663fd874109ed15d230599
chloedavn <104068806+chloedavn@users.noreply.github.com>
2022-05-31 18:01:43 +0200

    Update Essai.cpp

--- Essai.cpp
+++ Essai.cpp
@@ -1,49 +1,38 @@
 #pragma once
 #include <iostream>
-#include <sstream>
 #include <fstream>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include "Word.h"
 #include "Essai.h"
-#include "Solution.h"
 #include <string>
 //peut-être trop d'include mais on pourra supprimer le surplus plus tard
 using namespace std;
 
-void Essai::Colorier(Essai Mot)
+Essai::Essai(bool existe, const string &nom, const int &taille) :Word(nom, taille)
 {
-
+	existe_ = existe;
 }
 
-void Essai ::Comparer(Essai name_, Solution mot_compare)
-{
-	vector <int> tab(name_.getNom().size());
-	for (int i = 0; i < name_.getNom().size(); i++)
-	{
-		if (name_[i] == mot_compare[i])
-			tab[i] = 2;
-		if ((name_[i] != mot_compare[i]) && (mot_compare[i] == name_.getNom().find(mot_compare[i])))
-			tab[i] = 1;
-		else
-			tab[i] = 0;
-	}
-}
 
-void Essai::Existence(Essai Mot)
+void Essai :: Existence()
 {
-	int max = 323578;
 	string mot_compare;
 	ifstream file;
-	file.open("test.txt");
+	bool r = false;
+	file.open("liste_finale.txt");
 	if (!file.is_open())
+	{
 		cout << "Le fichier n existe pas " << endl;
+		system("pause");
+	}
 	else
-		while (getline(file, mot_compare)) //lit 1 par 1 les lignes du fichier et met cette ligne dans mot_compare
+		while (file >> mot_compare)
 		{
-			if (mot_compare == Mot.getNom()) //on compare le mot de la ligne au mot testé
-			break;
+			if (mot_compare == getNom()) //on compare le mot de la ligne au mot testé
+				existe_ = true;
 		}
-	cout << "Ce mot n existe pas !" << endl; //si aucun des mots du fichier n'est égal au mot testé alors le mot testé n'existe pas
+}
+
+
+void Essai::operator=(Word &w)
+{
+	existe_ = false;
 }
