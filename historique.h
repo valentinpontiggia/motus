7c502bb9269e337ddfb9dd270c2aabbc61a8886f
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-31 17:29:57 +0200

    Update historique.h

--- historique.h
+++ historique.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+
 #include "SFML/Graphics.hpp"
 using namespace sf;
 using namespace std;
@@ -7,15 +8,14 @@ using namespace std;
 class Historique
 {
 	Font font;
-	Text historique[4];
+	Text historique[7];//Un pour le titre, un pour la première ligne, un pour le retour au menu et un pour chaque partie affichée
 
 public:
-	Historique(float hauteur, float largeur, float moyenne);
+	Historique(float hauteur, float largeur); //construit l'historique et remplit les champs des historique[i]
 	~Historique();
 
-	void ecrireHistorique(int nb_essais);
-	void afficher(RenderWindow& window);
-	void afficherHistorique(Historique historique, RenderWindow& window);
-	double calculerMoyenne();
-
+	void ecrireHistorique(string mot, int nb_essais, bool victoire); //remplit le fichier "historique.txt" avec les informations de la dernière partie jouée 
+	vector<string> enregistrerHisto();
+	void afficher(RenderWindow& window); //draw les champs de l'historique dans la fenêtre, sans l'afficher.
+	void afficherHistorique(Historique historique, RenderWindow& window); //ouvre la fenêtre contenant l'historique et gère les clics de la souris pour retourner au menu
 };
