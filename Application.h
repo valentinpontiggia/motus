d99eb6857c742b8add9525b907a44b9b921209dc
chloedavn <104068806+chloedavn@users.noreply.github.com>
2022-05-31 18:02:46 +0200

    Create Application.h

---
+++ Application.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include"word.h"
+#include "Dessiner.h"
+#include"Solution.h"
+#include"Essai.h"
+using namespace std;
+using namespace sf;
+
+class Application
+{
+public:
+	Application();
+	void Affichage(RenderWindow &window, Solution sol);
+};
