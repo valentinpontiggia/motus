8be6f2056d9ce57820ef3db05c248842c59560ef
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-31 22:18:24 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -2,17 +2,19 @@
 #include "SFML/Graphics.hpp"
 using namespace sf;
 
+// Valentin PONTIGGIA
+
 class Menu
 {
 	Font font;
-	Text menu[4]; //Un pour le titre et un pour chaque option du menu
+	Text menu[4]; // Un pour le titre et un pour chaque option du menu
 
 public:
-	Menu(float hauteur, float largeur); //construit le menu et remplit les champs des menu[i]
+	Menu(float hauteur, float largeur); // Constructeur de la classe Menu : remplit les champs des menu[i]
 	~Menu();
-	void afficher(RenderWindow& window); //draw les champs du menu dans la fenêtre, sans l'afficher.
-	void afficherMenu(Menu menu); //ouvre la fenêtre contenant le menu et gère les clics de la souris pour ouvrir les différentes options du menu
+	void afficher(RenderWindow& window); // Dessine les champs du menu dans la fenêtre, sans l'afficher.
+	void afficherMenu(Menu menu); // Ouvre la fenêtre contenant le menu et gère les clics de la souris pour ouvrir les différentes options du menu
 
 };
 
-Text textPointed(RenderWindow& window, Text& text); //change la couleur du texte s'il est pointé (ne fonctionne pas)
+Text textPointed(RenderWindow& window, Text& text); // Change la couleur du texte s'il est pointé (ne fonctionne pas)
