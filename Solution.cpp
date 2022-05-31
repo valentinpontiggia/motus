8248064ccda17fe74f3a24783b647ecd9dfd75bc
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-31 23:07:40 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -9,17 +9,15 @@ using namespace std;
 
 void Solution::tirerAleatoirement()
 {
-	int max = 323578;
-	int indice_voulu = rand() % max;
-	int indice_actuel = 0;
+	int max = 323578; // Nombre de lignes du fichier liste_finale.txt
+	int indice_voulu = rand() % max; // Sélectionne un indice aléatoire qui sera la ligne du mot dans le dictionnaire
+	int indice_actuel = 0; // Correspond à l'indice de la ligne du dictionnaire actuellement lue
 	int i = 0;
-	//cout << "indice voulu : " << indice_voulu << endl;
-	string mot_actuel;
-	int taille;
+	string mot_actuel; // Prend une par une les valeurs des mots du dictionnaire jusqu'à celui désiré
 	ifstream file;
-	file.open("liste_finale.txt");
+	file.open("liste_finale.txt"); // Ouvre le fichier en lecture dans file
 	if (!file.is_open())
-		cout << "Le fichier n existe pas " << endl;
+		cout << "Le fichier n existe pas " << endl; // Vérification que le fichier est bien ouvert
 	else
 	{
 		while (getline(file, mot_actuel)) //lit 1 par 1 les lignes du fichier et met cette ligne dans mot actuel
@@ -27,17 +25,17 @@ void Solution::tirerAleatoirement()
 			if (indice_voulu < 323578)
 			{
 				indice_actuel++;
-				if (indice_actuel == indice_voulu) //si on tombe sur l'indice aléatoire voulu
+				if (indice_actuel == indice_voulu) // Si on tombe sur l'indice aléatoire voulu
 				{
-					if (mot_actuel.size() != 6)
+					if (mot_actuel.size() != 6) // On ne prend que les mots de 6 lettres
 					{
 						i = i + indice_voulu;
 						if ((max - i) <= 0)
 						{
 							i = 0;
 						}
-						indice_voulu = rand() % (max - i);
-						indice_actuel = 0;
+						indice_voulu = rand() % (max - i); // Recherche un nouveau mot comme solution
+						indice_actuel = 0; // On remet les valeurs à 0
 
 					}
 					else
