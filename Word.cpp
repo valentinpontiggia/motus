80c8460b164da23b8f51fc0ceed7569cfd369f7d
valentinpontiggia <98270960+valentinpontiggia@users.noreply.github.com>
2022-05-31 23:08:33 +0200

    Update Word.cpp

--- Word.cpp
+++ Word.cpp
@@ -12,9 +12,9 @@ Word::Word(string nom)
 }
 
 
-char& Word::operator[](int i)
+char& Word::operator[](int i) // Définition d'un opérateur [] pour accéder à une certaine lettre de notre Word.
 {
-	if (i < 0 || i >= nom_.size())
+	if (i < 0 || i >= nom_.size()) // L'indice doit être compris entre 0 et la taille du mot
 	{
 		cerr << "Dépassement d'indice : i = " << i;
 		abort();
@@ -23,24 +23,23 @@ char& Word::operator[](int i)
 }
 
 
-void Word::afficher() const
+void Word::afficher() const // Affiche le mot
 {
 	for (int i = 0; i < nom_.size(); i++)
 	{
-		cout << nom_[i];
+		cout << nom_[i]; // Caractère par caractère. Cette méthode nous a beaucoup servi pour la vérification de la validité de nos programmes
 	}
 	cout << endl;
 }
 
 
-void Word::MinToMaj() // fonction qui fait conerti une chaine minuscule en majuscule
+void Word::MinToMaj() // Fonction qui convertit une chaine minuscule en majuscule
 {
 	for (int i = 0; nom_[i] != '\0'; i++)
 	{
-		if (nom_[i] >= 'a' && nom_[i] <= 'z')
+		if (nom_[i] >= 'a' && nom_[i] <= 'z') // Si la lettre est bien une lettre de l'alphabet en minuscule
 		{
-			nom_[i] = nom_[i] - 32;
+			nom_[i] = nom_[i] - 32; // On la passe en majuscule (code ASCII)
 		}
 	}
 }
-
