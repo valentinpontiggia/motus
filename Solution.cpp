93e4072b1e420f6c39a953a000f6892627c626be
chloedavn <104068806+chloedavn@users.noreply.github.com>
2022-05-31 17:59:34 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,42 +1,55 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include "time.h"
-#include "Word.h"
-#include "Essai.h"
+#include <ctime>
 #include "Solution.h"
 #include <string>
-
 using namespace std;
 
-Solution Solution::tirerAleatoirement()
+
+void Solution::tirerAleatoirement()
 {
-	srand(time(NULL));
-	int max = 12;//323578
-	int indice_voulu = 1 + rand() % max;
+	int max = 323578;
+	int indice_voulu = rand() % max;
 	int indice_actuel = 0;
-	cout << "indice voulu : " << indice_voulu << endl;
+	int i = 0;
+	//cout << "indice voulu : " << indice_voulu << endl;
 	string mot_actuel;
 	int taille;
-	Solution mot_final;
 	ifstream file;
-	file.open("test.txt");
+	file.open("liste_finale.txt");
 	if (!file.is_open())
 		cout << "Le fichier n existe pas " << endl;
 	else
-		//while (file >> mot_actuel)
+	{
 		while (getline(file, mot_actuel)) //lit 1 par 1 les lignes du fichier et met cette ligne dans mot actuel
 		{
-			indice_actuel++;
-			if (indice_actuel == indice_voulu) //si on tombe sur l'indice aléatoire voulu
-			{	
-				mot_final.setNom(mot_actuel); //notre Solution mot_final voit son champ nom_ (de type string) = ligne lue actuellement
-				taille = mot_actuel.size(); 
-				mot_final.setTaille(taille); // Solution mot_final voit son champ taille_ = taille du mot lu actuellement
-				cout << mot_final.getNom() << endl; //ici ça affiche bien le mot, c'est cette ligne qui fonctionne pas dans le main
-				break;
+			if (indice_voulu < 323578)
+			{
+				indice_actuel++;
+				if (indice_actuel == indice_voulu) //si on tombe sur l'indice aléatoire voulu
+				{
+					if (mot_actuel.size() != 6)
+					{
+						i = i + indice_voulu;
+						if ((max - i) <= 0)
+						{
+							i = 0;
+						}
+						indice_voulu = rand() % (max - i);
+						indice_actuel = 0;
+
+					}
+					else
+					{
+						setNom(mot_actuel); //notre Solution mot_final voit son champ nom_ (de type string) = ligne lue actuellement
+						taille = mot_actuel.size();
+						setTaille(taille); // Solution mot_final voit son champ taille_ = taille du mot lu actuellement
+					}
+				}
 			}
+			else
+				indice_voulu = 0;
 		}
-	return mot_final;
+	}
 }
